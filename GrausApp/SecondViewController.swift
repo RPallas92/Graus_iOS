3c2cb81a6e245f3a20577c52fd3fac10db079d16
Ricardo Pallas <Ricardo.Pallas@adidas-group.com>
2017-04-08 18:20:56 +0200

    Added project structure

---
+++ GrausApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  GrausApp
+//
+//  Created by Pallas, Ricardo on 4/8/17.
+//  Copyright © 2017 Pallas, Ricardo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
