f98ee9957bec0bf18d5536b198f51c82553e81af
Ricardo Pallás <rpallas92@gmail.com>
2017-06-20 22:55:29 +0200

    Added state and Events for AgendaViewController

--- GrausApp/AgendaViewController.swift
+++ GrausApp/AgendaViewController.swift
@@ -8,7 +8,51 @@
 
 import UIKit
 
+
+fileprivate struct State {
+    var results: [AgendaEvent]
+    var lastError: ApiError?
+    var shouldLoadData = true
+}
+
+fileprivate enum Event {
+    case startLoadingEvents()
+    case response(LoadAgendaEventsResponse)
+}
+
+
+// transitions
+extension State {
+    static var empty: State {
+        return State( results: [], lastError: nil, shouldLoadData: true)
+    }
+    static func reduce(state: State, event: Event) -> State {
+        switch event {
+        case .startLoadingEvents():
+            var result = state
+            result.shouldLoadData = true
+            return result
+        case .response(.success(let response)):
+            var result = state
+            result.results += response
+            result.lastError = nil
+            return result
+        case .response(.failure(let error)):
+            var result = state
+            result.lastError = error
+            return result
+        }
+    }
+}
+
+// queries
+extension State {
+    
+}
+
 class AgendaViewController: UIViewController {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
